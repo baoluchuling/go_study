7d0707acc1c63ba88cbc7d15e71df4bcb2d3b5ca
baoluchuling <baoluchuling@126.com>
2022-12-14 16:25:00 +0800

    update

--- webserver/main.go
+++ webserver/main.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // 解析参数，默认是不会解析的
-	fmt.Println(r.Form) // 这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "Hello World! Append a name to the URL to say hello. For example, use %s/Mary to say Hello to Mary", r.Host)
+	} else {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	}
-	fmt.Fprintf(w, "Hello astaxie!") // 这个写入到 w 的是输出到客户端的
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       // 设置访问的路由
-	err := http.ListenAndServe(":9090", nil) // 设置监听的端口
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		fmt.Printf("%s", err)
 	}
 }
